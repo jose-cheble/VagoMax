4512fda994cd2c22866275092e169f7ce5b53d72
Jose Cheble <josecheble@gmail.com>
2022-04-13 15:46:31 -0300

    enviroment variables added
    the django_secret key was changed to an enviroment variable

--- VagoMax/settings.py
+++ VagoMax/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@t23l^^%665f8+@$p-+)rsggkokoy-(cmnz2sb3sp&@fndq6(d'
+SECRET_KEY = os.getenv("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
